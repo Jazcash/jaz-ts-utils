0edd1a73b9a168de809422f7c97a3aee596b093f
Jazcash <jaspercashmore@gmail.com>
2021-01-17 11:22:59 +0000

    added optionals util type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 export type Exclusive<A extends object, B extends object> = Omit<A, keyof B> & Omit<B, keyof A>;
 export type Inclusive<A extends object, B extends object> = Omit<A & B, keyof Exclusive<A, B>>;
 export type Merge<A extends object, B extends object> = Inclusive<A, B> & Partial<Exclusive<A, B>>;
+
+export type OptionalPropertiesOf<T extends object> = Exclude<{ [K in keyof T]: T extends Record<K, T[K]> ? never : K }[keyof T], undefined>;
+export type Optionals<T extends object> = Pick<T, OptionalPropertiesOf<T>>;
