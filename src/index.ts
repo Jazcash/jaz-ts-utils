34623b361a445870abf9b416a8eb00a6b7c6b52c
Jazcash <jaspercashmore@gmail.com>
2021-01-17 10:45:28 +0000

    added delay

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { delay } from "./delay";
 import { Signal, SignalBinding } from "./signal";
 import { Exclusive, Inclusive, Merge } from "./types";
 
 export { Signal, SignalBinding };
 
 export { Exclusive, Inclusive, Merge };
+
+export { delay }
