d1485fd34dac898a9c17d09abd1b021844861615
Jazcash <jaspercashmore@gmail.com>
2021-01-17 11:29:44 +0000

    export optionals

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { delay } from "./delay";
 import { Signal, SignalBinding } from "./signal";
-import { Exclusive, Inclusive, Merge } from "./types";
+import { Exclusive, Inclusive, Merge, Optionals } from "./types";
 
 export { Signal, SignalBinding };
 
-export { Exclusive, Inclusive, Merge };
+export { Exclusive, Inclusive, Merge, Optionals };
 
-export { delay }
+export { delay };
