f3750feb8e577ea24e8d121819f3a5b98da1226c
Jazcash <jaspercashmore@gmail.com>
2021-01-20 23:39:25 +0000

    optionals should be required

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,4 @@ export type Inclusive<A extends object, B extends object> = Omit<A & B, keyof Ex
 export type Merge<A extends object, B extends object> = Inclusive<A, B> & Partial<Exclusive<A, B>>;
 
 export type OptionalPropertiesOf<T extends object> = Exclude<{ [K in keyof T]: T extends Record<K, T[K]> ? never : K }[keyof T], undefined>;
-export type Optionals<T extends object> = Pick<T, OptionalPropertiesOf<T>>;
+export type Optionals<T extends object> = Required<Pick<T, OptionalPropertiesOf<T>>>;
