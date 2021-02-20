9a4f7df8e1fe9f66e4902f2e77887f58cdd183a4
Jazcash <jaspercashmore@gmail.com>
2021-02-20 23:31:02 +0000

    added writeable and deepwritable types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { delay } from "./delay";
 import { Signal, SignalBinding } from "./signal";
-import { Exclusive, Inclusive, Merge, Optionals } from "./types";
+import { DeepWriteable, Exclusive, Inclusive, Merge, Optionals, Writeable } from "./types";
 
 export { Signal, SignalBinding };
 
-export { Exclusive, Inclusive, Merge, Optionals };
+export { Exclusive, Inclusive, Merge, Optionals, Writeable, DeepWriteable };
 
 export { delay };
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,6 @@ export type Merge<A extends object, B extends object> = Inclusive<A, B> & Partia
 
 export type OptionalPropertiesOf<T extends object> = Exclude<{ [K in keyof T]: T extends Record<K, T[K]> ? never : K }[keyof T], undefined>;
 export type Optionals<T extends object> = Required<Pick<T, OptionalPropertiesOf<T>>>;
+
+export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
+export type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> };
