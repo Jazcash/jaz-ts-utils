375d15217cc56afc414e7f5215de28feb0b5ebd3
Jazcash <jaspercashmore@gmail.com>
2021-06-13 12:55:44 +0100

    cleaner exports and added deep partial

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import { delay } from "./delay";
-import { Signal, SignalBinding } from "./signal";
-import { DeepWriteable, Exclusive, Inclusive, Merge, Optionals, Writeable } from "./types";
-
-export { Signal, SignalBinding };
-
-export { Exclusive, Inclusive, Merge, Optionals, Writeable, DeepWriteable };
-
-export { delay };
+export * from "./delay";
+export * from "./signal";
+export * from "./types";
--- src/types.ts
+++ src/types.ts
@@ -5,5 +5,7 @@ export type Merge<A extends object, B extends object> = Inclusive<A, B> & Partia
 export type OptionalPropertiesOf<T extends object> = Exclude<{ [K in keyof T]: T extends Record<K, T[K]> ? never : K }[keyof T], undefined>;
 export type Optionals<T extends object> = Required<Pick<T, OptionalPropertiesOf<T>>>;
 
-export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
-export type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> };
+export type Writable<T> = { -readonly [P in keyof T]: T[P] };
+export type DeepWritable<T> = { -readonly [P in keyof T]: DeepWritable<T[P]> };
+
+export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> };
