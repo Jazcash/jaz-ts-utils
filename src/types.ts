fa5fe08d2b75e8095d474067d41049e220ae7c46
Jazcash <jaspercashmore@gmail.com>
2021-08-30 00:33:36 +0100

    immutable type

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,16 @@ export type Writable<T> = { -readonly [P in keyof T]: T[P] };
 export type DeepWritable<T> = { -readonly [P in keyof T]: DeepWritable<T[P]> };
 
 export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> };
+
+type ImmutablePrimitive = undefined | null | boolean | string | number | Function;
+
+export type Immutable<T> =
+    T extends ImmutablePrimitive ? T :
+    T extends Array<infer U> ? ImmutableArray<U> :
+    T extends Map<infer K, infer V> ? ImmutableMap<K, V> :
+    T extends Set<infer M> ? ImmutableSet<M> : ImmutableObject<T>;
+
+export type ImmutableArray<T> = ReadonlyArray<Immutable<T>>;
+export type ImmutableMap<K, V> = ReadonlyMap<Immutable<K>, Immutable<V>>;
+export type ImmutableSet<T> = ReadonlySet<Immutable<T>>;
+export type ImmutableObject<T> = { readonly [K in keyof T]: Immutable<T[K]> };
