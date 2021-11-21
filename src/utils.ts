acc27f38d592822fe16a0186ececdb83907c31fd
Jazcash <jaspercashmore@gmail.com>
2021-11-21 11:21:37 +0000

    asArray and entries

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
 export function clamp(num: number, min: number, max: number) {
     return num <= min ? min : num >= max ? max : num;
 }
+
+export function asArray<T>(target: T | T[]): T[] {
+    if (Array.isArray(target)) {
+        return target;
+    } else {
+        return [target];
+    }
+}
+
+export type Entries<T> = { [K in keyof T]: [K, T[K]] }[keyof T];
+export function entries<T extends object>(t: T): Entries<T>[] {
+    return Object.entries(t) as any;
+}
