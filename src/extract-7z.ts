13b462d69230442216a117f036110c1a815003b7
Jazcash <jaspercashmore@gmail.com>
2022-08-28 17:34:59 +0100

    fix extract outdir

--- src/extract-7z.ts
+++ src/extract-7z.ts
@@ -1,11 +1,7 @@
 import { unpack } from "7zip-min";
-import * as path from "path";
 
-export function extract7z(archivePath: string, outputName: string) {
+export function extract7z(archivePath: string, outputPath: string) {
     return new Promise<void>((resolve, reject) => {
-        const archivePathObj = path.parse(archivePath);
-        const outputPath = path.join(archivePathObj.dir, outputName);
-
         unpack(archivePath, outputPath, (err) => {
             if (err) {
                 reject(err);
