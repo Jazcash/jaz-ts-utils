6063457e89dcd67a319a28bf23a769c1cf1f0325
Jazcash <jaspercashmore@gmail.com>
2023-07-12 06:35:27 +0100

    don't lowercase first

--- src/title-case.ts
+++ src/title-case.ts
@@ -1,6 +1,5 @@
 export function titleCase(str: string): string {
     return str
-        .toLowerCase()
         .split(" ")
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
         .join(" ");
